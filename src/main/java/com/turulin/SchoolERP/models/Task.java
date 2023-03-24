fb7698637c6f6c33bed6c2f514388ce3da13e780
aturulin <Aleksandr.Turulin@kauri-iot.com>
2023-03-24 11:40:19 +0300

    добавлена реактивная БД Postgres

--- src/main/java/com/turulin/SchoolERP/models/Task.java
+++ src/main/java/com/turulin/SchoolERP/models/Task.java
@@ -5,7 +5,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Data
-@Table
+@Table("tasks")
 public class Task {
     @Id
     private Long id;
